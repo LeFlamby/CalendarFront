f4de5013c6536fdfeead5cf8073ab3bb2a263767
pilou <p-l.b@hotmail.com>
2023-11-22 16:08:37 +0100

    updated validator

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validator, ValidatorFn, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
-import {Router} from '@angular/router'
+import { Router } from '@angular/router'
 import { DividerModule } from 'primeng/divider';
 
 
@@ -13,40 +13,42 @@ import { DividerModule } from 'primeng/divider';
 export class RegisterComponent {
 
   registerForm: FormGroup;
-  registerData:any;
-  isTyping : boolean = false
+  registerData: any;
+  isTyping: boolean = false
 
-  constructor( private formBuilder: FormBuilder, private auth: AuthService, private router: Router){
-    this.registerForm = this.formBuilder.group({
-      username: ['', [Validators.required, Validators.minLength(3)]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
-      confirmPassword: ['', [Validators.required, Validators.minLength(8)]],
-    }, {
-      validators: [this.passwordMatchesValidator]
-    });
+  constructor(private formBuilder: FormBuilder, private auth: AuthService, private router: Router) {
+
+    this.registerForm = new FormGroup(
+      {
+        username: new FormControl(
+          '', [Validators.required, Validators.minLength(3)]
+        ),
+        password: new FormControl(
+          '', [Validators.required, Validators.minLength(3)]
+        ),
+        confirmPassword: new FormControl(
+          '', [Validators.required]
+        ),
+      },
+      this.passwordMatch('password', 'confirmPassword')
+    )
+
     this.registerForm.get('confirmPassword')?.valueChanges.subscribe(() => {
       this.isTyping = true;
     });
+
   }
 
-  passwordMatchesValidator(form: FormGroup) {
-    const password = form.get('password')?.value;
-    const confirmPassword = form.get('confirmPassword')?.value;
-    if (password !== confirmPassword) {
-      return { passwordMismatch: true };
-    }
-    return null;
-  }
 
-  onRegisterSubmit(){
+  onRegisterSubmit() {
     if (this.registerForm.valid) {
       const { password, username } = this.registerForm.value;
-     
+
       this.auth.register({
         username: username,
         password: password,
       }).subscribe((data: any) => {
-        
+
         alert("Votre compte a bien été créé");
         this.router.navigate(['/login']);
         this.registerData = data;
@@ -54,6 +56,33 @@ export class RegisterComponent {
       });
     }
   }
+
+  passwordMatch(password: string, confirmPassword: string): ValidatorFn {
+    return (formGroup: AbstractControl): { [key: string]: any } | null => {
+      const passwordControl = formGroup.get(password);
+      const confirmPasswordControl = formGroup.get(confirmPassword);
+
+      if (!passwordControl || !confirmPasswordControl) {
+        return null;
+      }
+
+      if (
+        confirmPasswordControl.errors &&
+        !confirmPasswordControl.errors["passwordMismatch"]
+      ) {
+        return null;
+      }
+
+      if (passwordControl.value !== confirmPasswordControl.value) {
+        confirmPasswordControl.setErrors({ passwordMismatch: true });
+        return { passwordMismatch: true };
+      } else {
+        confirmPasswordControl.setErrors(null);
+        return null;
+      }
+    };
+  }
+
 }
 
 
